60c120e99b56cf0f486614e231574777c9251ad1
Guo Ren <guoren@linux.alibaba.com>
2020-07-03 09:51:40 +0000

    riscv: Add uprobes supported
    This patch adds support for uprobes on riscv architecture.
    
    Just like kprobe, it support single-step and simulate instructions.
    
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>
    Reviewed-by: Pekka Enberg <penberg@kernel.org>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Palmer Dabbelt <palmerdabbelt@google.com>

--- arch/riscv/mm/fault.c
+++ arch/riscv/mm/fault.c
@@ -230,6 +230,7 @@ asmlinkage void do_page_fault(struct pt_regs *regs)
 	 * in an atomic region, then we must not take the fault.
 	 */
 	if (unlikely(faulthandler_disabled() || !mm)) {
+		tsk->thread.bad_cause = cause;
 		no_context(regs, addr);
 		return;
 	}
@@ -247,16 +248,19 @@ retry:
 	mmap_read_lock(mm);
 	vma = find_vma(mm, addr);
 	if (unlikely(!vma)) {
+		tsk->thread.bad_cause = cause;
 		bad_area(regs, mm, code, addr);
 		return;
 	}
 	if (likely(vma->vm_start <= addr))
 		goto good_area;
 	if (unlikely(!(vma->vm_flags & VM_GROWSDOWN))) {
+		tsk->thread.bad_cause = cause;
 		bad_area(regs, mm, code, addr);
 		return;
 	}
 	if (unlikely(expand_stack(vma, addr))) {
+		tsk->thread.bad_cause = cause;
 		bad_area(regs, mm, code, addr);
 		return;
 	}
@@ -269,6 +273,7 @@ good_area:
 	code = SEGV_ACCERR;
 
 	if (unlikely(access_error(cause, vma))) {
+		tsk->thread.bad_cause = cause;
 		bad_area(regs, mm, code, addr);
 		return;
 	}
@@ -302,6 +307,7 @@ good_area:
 	mmap_read_unlock(mm);
 
 	if (unlikely(fault & VM_FAULT_ERROR)) {
+		tsk->thread.bad_cause = cause;
 		mm_fault_error(regs, addr, fault);
 		return;
 	}
